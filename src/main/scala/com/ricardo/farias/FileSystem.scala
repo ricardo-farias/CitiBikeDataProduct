526973b4620cee5831d8cf9a7820bf5287ae77c3
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-09-26 16:06:37 -0500

    [Ricardo][#64] Removed the dependency from the code, successfully spark-submit and populated glue and s3

--- src/main/scala/com/ricardo/farias/FileSystem.scala
+++ src/main/scala/com/ricardo/farias/FileSystem.scala
@@ -2,9 +2,6 @@ package com.ricardo.farias
 
 import java.io.File
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.S3Object
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.catalyst.parser.LegacyTypeStringParser
 import org.apache.spark.sql.types.{DataType, StructType}
@@ -113,14 +110,9 @@ object LocalFileSystem extends FileSystem {
 }
 
 object S3FileSystem extends FileSystem {
-  private val cred = new ProfileCredentialsProvider("profile sparkapp")
-  private val s3Client = AmazonS3ClientBuilder.standard()
-    .withCredentials(cred).build()
   private val bucket = Constants.bucket
 
   override def readCsv(schema : StructType, filePath: String, filename: String, timeStampFormat: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
-    val file : S3Object = getObject(filePath, filename)
-    println(s"s3a://${file.getBucketName}/${file.getKey}")
     val df = sparkSession.read.format("csv")
       .options(
         Map(
@@ -133,41 +125,37 @@ object S3FileSystem extends FileSystem {
           "columnNameOfCorruptRecord"->"Corrupted"
         ))
       .schema(schema)
-      .load(s"s3a://${file.getBucketName}/${file.getKey}")
+      .load(s"s3a://${bucket}/${filePath}/${filename}")
     val badDF = df.filter(df.col("Corrupted").isNotNull).toDF
     val goodDF = df.filter(df.col("Corrupted").isNull).toDF
     (goodDF, badDF)
   }
 
   override def schemalessReadCsv(filePath: String, filename: String)(implicit sparkSession: SparkSession): DataFrame = {
-    val file : S3Object = getObject(filePath, filename)
-    println(s"s3a://${file.getBucketName}/${file.getKey}")
     val df = sparkSession.read.format("csv")
       .options(Map(
         "header"->"true",
         "inferschema"->"true",
         "dateFormat"-> "MM/dd/yyyy","timestampFormat"->"MM/dd/yyyy hh:mm:ss a",
         "ignoreTrailingWhiteSpace"->"true",
-        "ignoreLeadingWhiteSpace"->"true")).load(s"s3a://${file.getBucketName}/${file.getKey}")
+        "ignoreLeadingWhiteSpace"->"true")).load(s"s3a://${bucket}/${filePath}/${filename}")
     df
   }
 
   override def readJson(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
-    val file : S3Object = getObject(filePath, filename)
     val df = sparkSession.read.options(
       Map("dateFormat"->"MM/dd/yy",
         "timestampFormat"->"yyyy-MM-ssThh:mm:ss.",
         "columnNameOfCorruptRecord"->"Corrupted",
         "nullValues"->"NULL"))
       .schema(schema)
-      .json(f"s3a://${file.getBucketName}/${file.getKey}")
+      .json(f"s3a://${Constants.bucket}/${filePath}/${filename}")
     val badDF = df.filter(df.col("Corrupted").isNotNull).toDF
     val goodDF = df.filter(df.col("Corrupted").isNull).toDF
     (goodDF, badDF)
   }
 
   override def readJsonForStationData(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
-    val file : S3Object = getObject(filePath, filename)
     val flattenedDF = sparkSession.read.options(
       Map("dateFormat"->"MM/dd/yy",
         "columnNameOfCorruptRecord"->"Corrupted",
@@ -176,7 +164,7 @@ object S3FileSystem extends FileSystem {
         "nullValues"->"NULL"
       ))
       .schema(schema)
-      .json(f"s3a://${file.getBucketName}/${file.getKey}")
+      .json(f"s3a://${bucket}/${filePath}/${filename}")
       .selectExpr("explode(stations) as station_array","*").select("station_array.*","Corrupted")
       .select("extra.*","*").drop("extra").withColumnRenamed("last_updated","last_updated_tmp")
       .selectExpr("from_unixtime(last_updated_tmp) as last_updated","*").drop("last_updated_tmp")
@@ -187,11 +175,7 @@ object S3FileSystem extends FileSystem {
     }
 
   override def readSchemaFromJson(filePath: String, filename: String)(implicit sparkContext: SparkContext) : StructType = {
-    val file : S3Object = getObject(filePath, filename)
-    val content = file.getObjectContent
-    println(content)
-    println(file.getKey)
-    val source = sparkContext.textFile(f"s3a://${file.getBucketName}/${file.getKey}").collect()
+    val source = sparkContext.textFile(f"s3a://${bucket}/${filePath}/${filename}").collect()
 
     val data = source.toList.mkString("\n")
     val schema  = Try(DataType.fromJson(data)).getOrElse(LegacyTypeStringParser.parse(data)) match {
@@ -203,7 +187,7 @@ object S3FileSystem extends FileSystem {
 
   override def write(filename: String, data: DataFrame, filePath: String)(implicit sparkSession: SparkSession) : Unit = {
     val name = if (filename.contains("/")) filename.split("/")(1) else filename
-    data.createOrReplaceTempView(name)
+    data.createOrReplaceTempView(filename)
     //    sparkSession.sql(s"""CREATE TABLE default.${name}
     //      USING PARQUET
     //      OPTIONS ('compression'='snappy')
@@ -216,15 +200,8 @@ object S3FileSystem extends FileSystem {
       .saveAsTable(s"${Constants.database}.${name}")
   }
 
-  private def getObject(filePath: String, filename: String) : S3Object = {
-    val result = s3Client.getObject(bucket, f"${filePath}/${filename}")
-    result
-  }
-
   override def listObjects() : Unit = {
-    val result = s3Client.listObjectsV2(bucket)
-    val objects = result.getObjectSummaries.toArray
-    objects.foreach(println)
+    // TODO
   }
 
 }
